e357c698caacb046fa2fe0432dcc79a6c0e89bb5
danny <qkrwngur5185@naver.com>
2025-01-28 12:59:56 +0900

    tailwind css 추가

--- src/pages/auth/MemberSave.jsx
+++ src/pages/auth/MemberSave.jsx
@@ -1,8 +1,17 @@
-const MemberSave = () => (
-    <div>
-        <h1>Member Save</h1>
-        <p>This is the Login page.</p>
-    </div>
-);
+
+const MemberSave = () => {
+
+    return (
+        <div>
+            <h1>안녕하세요</h1>
+            <div>
+                <input type="text" className="border p-2 w-full" />
+            </div>
+            <div>
+                <input type="text" className="border p-2 w-full" />
+            </div>
+        </div>
+    );
+};
 
 export default MemberSave;
